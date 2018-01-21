7174e951e82fc7280e585e640623152e69f08386
keppieemeli <35644547+keppieemeli@users.noreply.github.com>
2018-01-21 23:52:59 +0200

    Car Update
    Some car update

---
+++ Assets/CarMain.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMain : MonoBehaviour {
+    private float distance;
+    public float Speed = 10f;
+    private float maxDis = 5f;
+    public GameObject BackTierRight;
+    public GameObject BackTierLeft;
+    
+    public bool InCar = false;
+    public bool InCarRange = false;
+    private float y = 0f;
+    private float z = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        
+        var fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, maxDis)) {
+            InCarRange = true;
+            if (Input.GetKeyDown(KeyCode.E) && InCar == false)
+            {
+                InCar = true;
+            }
+        }
+        else
+        {
+            InCarRange = false;
+        }
+        if (Input.GetKeyDown(KeyCode.G) && InCar == true)
+        {
+            InCar = false;
+        }
+
+
+
+            if (Input.GetKey(KeyCode.W) && InCar == true)
+        {
+            BackTierLeft.transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+            BackTierRight.transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+            
+        }
+
+
+
+
+
+
+	}
+}
---
+++ Assets/newCar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class newCar : MonoBehaviour {
+    public GameObject ThisCar;
+    public bool Incar;
+    public bool InCarRange;
+    private float distance;
+    private float maxDis = 7f;
+    public GameObject CameraPath;
+    public GameObject FPSHuman;
+    public GameObject PlayerTemplocation;
+	// Use this for initialization
+	void Start () {
+        ThisCar.GetComponent<CarController>().enabled = false;
+        ThisCar.GetComponent<CarAudio>().enabled = false;
+        ThisCar.GetComponent<CarUserControl>().enabled = false;
+        CameraPath.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var fwd = ThisCar.transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        distance = Vector3.Distance(ThisCar.transform.position, Camera.main.transform.position);
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, maxDis)) {
+            InCarRange = true;
+            if (Input.GetKeyDown(KeyCode.E) && Incar == false)
+            {
+                Incar = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.G) && Incar == true)
+        {
+            Incar = false;
+            FPSHuman.transform.position = ThisCar.transform.position + new Vector3(0, 5, 0);
+        }
+        if (Incar == true)
+        {
+            ThisCar.GetComponent<CarController>().enabled = true;
+            ThisCar.GetComponent<CarAudio>().enabled = true;
+            ThisCar.GetComponent<CarUserControl>().enabled = true;
+            CameraPath.SetActive(true);
+            FPSHuman.transform.position = PlayerTemplocation.transform.position;
+            FPSHuman.SetActive(false);
+            
+
+
+
+
+
+        }
+        if(Incar == false)
+        {
+            ThisCar.GetComponent<CarController>().enabled = false;
+            ThisCar.GetComponent<CarAudio>().enabled = false;
+            ThisCar.GetComponent<CarUserControl>().enabled = false;
+            FPSHuman.SetActive(true);
+            CameraPath.SetActive(false);
+            FPSHuman.SetActive(true);
+        }
+
+
+    }
+}
